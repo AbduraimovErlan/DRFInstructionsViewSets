02acf5c349c71f4fed7454c31a5b82e45038663e
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2023-06-20 15:03:20 -0400

    ModelViewSet

---
+++ ModelViewSet/views.py
@@ -0,0 +1,31 @@
+from rest_framework.viewsets import ModelViewSet
+
+
+""" Attributes """
+"""
+
+allowed_methods = <property objects
+authentication_classes = [<class
+content_negotiation_class = <class
+default_response_headers = <property object
+filter_backends = []
+http_method_names = []
+lookup_field = 'pk'
+lookup_ulr_kwarg = None
+metadata_class = <class
+pagination_class = None
+paginator = <property object
+parser_classes
+queryset = None
+renderer_classes = [<class
+schema = 
+serializer_classes = None
+settings = 
+throttle_classes = []
+versioning_class = None
+view_is_async = False
+
+
+"""
+
+""" Methods """
